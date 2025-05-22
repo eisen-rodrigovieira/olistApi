8fc71e4f1d5c6d94ea1ab46698ab4575ebabc72f
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-05-22 09:07:21 -0300

    incluido path arquivos pedido

--- params/configSankhya.py
+++ params/configSankhya.py
@@ -1,3 +1,7 @@
-PATH_SCRIPT_PRODUTO = "sql/dql/produto.sql"
-PATH_UPDATE_PRODUTO = "sql/dml/upd_produto.sql"
+PATH_SCRIPT_PRODUTO        = "sql/dql/produto.sql"
+PATH_UPDATE_PRODUTO        = "sql/dml/upd_produto.sql"
 PATH_PARAMS_UPDATE_PRODUTO = "json/dml/upd_produto.json"
+
+PATH_SCRIPT_PEDIDO_CAB     = "sql/dql/pedido.sql"
+PATH_SCRIPT_PEDIDO_ITE     = "sql/dql/pedido_item.sql"
+PATH_SCRIPT_PEDIDO_FIN     = "sql/dql/pedido_parcela.sql"
