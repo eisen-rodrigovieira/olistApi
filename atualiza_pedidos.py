a8c10c641a601615880a1847e0979fc17868a19a
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-07-04 10:53:29 -0300

    funcoes separadas por status do pedido

--- atualiza_pedidos.py
+++ atualiza_pedidos.py
@@ -4,5 +4,7 @@ from src.app.app import App
 app_pedido = App().Pedido()
 
 if __name__=='__main__':
-    asyncio.run(app_pedido.busca_novos())
+    asyncio.run(app_pedido.importa_aprovados())
+    asyncio.run(app_pedido.importa_prep_envio())
+    asyncio.run(app_pedido.importa_faturados())
 
