b5e6552693e753fc7cc35d7483a74aebae867d82
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-07-08 14:10:07 -0300

    Remover arquivos agora ignorados pelo .gitignore

--- atualiza_estoque.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from src.app.app import App
-
-app_estoque = App().Estoque()
-
-if __name__=='__main__':
-    asyncio.run(app_estoque.atualizar())
-
--- atualiza_produtos.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from src.app.app import App
-
-app_produto = App().Produto()
-
-if __name__=='__main__':
-    asyncio.run(app_produto.ol_atualizar_produtos())
-    asyncio.run(app_produto.snk_atualizar_produtos())
-
