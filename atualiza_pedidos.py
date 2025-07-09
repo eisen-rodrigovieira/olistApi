f64653b223daffbdcc1ea4a23eb50a98a64f92f9
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-07-09 17:23:18 -0300

    adicionado importa_pronto_envio

--- atualiza_pedidos.py
+++ atualiza_pedidos.py
@@ -6,5 +6,6 @@ app_pedido = App().Pedido()
 if __name__=='__main__':
     asyncio.run(app_pedido.importa_aprovados())
     asyncio.run(app_pedido.importa_prep_envio())
+    asyncio.run(app_pedido.importa_pronto_envio())
     asyncio.run(app_pedido.importa_faturados())
 
